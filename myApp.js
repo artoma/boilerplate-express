0030de5b8ed1ce8c826567cff826ebde2ee5876f
artoma <artoma@gmail.com>
2023-04-10 11:48:32 +0300

    fix: middleware static path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 absolutePath = __dirname + '/views/index.html'
 app.get('/', (req, res) => {
  res.sendFile(absolutePath);
