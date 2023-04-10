1ddde09b07c70468f31c66dce3613aabe4ecd609
artoma <artoma@gmail.com>
2023-04-10 11:39:11 +0300

    feat: middleware static path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-console.log("Hello World")
+app.use(express.static(__dirname + '/public'));
 absolutePath = __dirname + '/views/index.html'
 app.get('/', (req, res) => {
  res.sendFile(absolutePath);
